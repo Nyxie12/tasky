7fd7572d7e24131b55e7516f53660476ec11fc00
Nyxie12 <keith.p.john@gmail.com>
2020-04-15 00:35:15 -0400

    w

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:tasky/loginpage.dart';
 import 'package:tasky/splashscreen.dart';
 
+// change
+
 void main() => runApp(MaterialApp(
   theme: 
     ThemeData(primaryColor: Colors.red, accentColor: Colors.yellow),
